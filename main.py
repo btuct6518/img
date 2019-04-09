5f6bf45844ab875181cf35188008d485fe942b9e
LEO <asda2753596@gmail.com>
2019-04-09 16:29:18 +0800

    add pictures

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image 
 import os
 
-
-
-image_file = Image.open("curry.png") # open colour image
-image_file = image_file.convert('L') # convert image to black and white
-image_file.save('result.png')
+for file in os.listdir('.'):
+	if file.endswith('.jpg'):
+		image_file = Image.open(file) # open colour image
+		image_file = image_file.convert('L') # convert image to black and white
+		image_file.save(file[:-4] + '-player.png')
